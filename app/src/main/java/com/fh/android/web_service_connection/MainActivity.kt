4bc1acecf1e4d869a05191f2001bd464fe4f0798
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-03-23 09:28:54 +0100

    Code clean up and ui update

--- app/src/main/java/com/fh/android/web_service_connection/MainActivity.kt
+++ app/src/main/java/com/fh/android/web_service_connection/MainActivity.kt
@@ -12,13 +12,24 @@ import com.google.gson.reflect.TypeToken
 import models.Cat
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
+    // Provide your api link here:
     private val url : String = "https://api.myjson.com/bins/1b9u6m"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var btLoad : Button? = null
+    private var btReset : Button? = null
+    private var tvResult : TextView? = null
+    private var progressBar : ProgressBar? = null
 
+    var dataLoaded:Boolean by Delegates.observable(false){
+            _, _, dataLoaded ->
+        btReset?.isEnabled = dataLoaded
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         //Thread policy for internet access
         val SDK_INT = android.os.Build.VERSION.SDK_INT
         if (SDK_INT > 8) {
@@ -26,51 +37,68 @@ class MainActivity : AppCompatActivity() {
                 .permitAll().build()
             StrictMode.setThreadPolicy(policy)
         }
-        //CreateDemoData()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //kotlin typecasting of view
-        val button = findViewById(R.id.button) as Button
-        var tv = findViewById(R.id.mytext) as TextView
-        val progress_bar = findViewById(R.id.progressBar) as ProgressBar
-        progress_bar.visibility = View.INVISIBLE
+        // Load UI elements
+        btLoad = findViewById(R.id.btLoad)
+        btReset = findViewById(R.id.btReset)
+        tvResult = findViewById(R.id.mytext)
+        progressBar = findViewById(R.id.progressBar)
 
-        //on button click event
-        button.setOnClickListener {
-            progress_bar.visibility = View.VISIBLE
+        // Prepare UI
+        tvResult?.text = ""
+        progressBar?.visibility = View.INVISIBLE
 
-            // Kotlin + HttpURLConnection
-            var text : String? = null
-            try{
-                val connection = URL(url).openConnection() as HttpURLConnection
-                connection.connect()
-                println(connection.responseCode)
-                println(connection.getHeaderField("Content-Type"))
+        dataLoaded = false
 
-                text = connection.inputStream.use { conn ->
-                    conn.reader().use { reader -> reader.readText() }
-                }
+        // Set EventHandler
+        btLoad?.setOnClickListener { onLoadData() }
+        btReset?.setOnClickListener{ onReset() }
+    }
 
-            } catch (ex : Exception) {
-                tv.setText(ex.toString())
+    private fun onLoadData(){
+        btLoad?.isEnabled = false
+        progressBar?.visibility = View.VISIBLE
+
+        var resultText : String = loadApiData()
+        tvResult?.text = resultText
+
+        progressBar?.visibility = View.INVISIBLE
+        btLoad?.isEnabled = true
+    }
+
+    private fun loadApiData() : String {
+        return try {
+            val connection = URL(url).openConnection() as HttpURLConnection
+            connection.connect()
+
+            val jsonString = connection.inputStream.use { conn ->
+                conn.reader().use { reader -> reader.readText() }
             }
+            var deserializedString = deserializeJsonString(jsonString)
+            dataLoaded = true
 
-            // Json
-            val catType = object : TypeToken<List<Cat>>() {}.type
-            val cats = Gson().fromJson<List<Cat>>(text, catType)
-            tv.text = cats.joinToString()
-
-            progress_bar.visibility = View.GONE
+            deserializedString
+        } catch (ex : Exception) {
+            "Error while accessing API $url. The following error occurred: $ex"
         }
     }
 
-    private fun createDemoData(){
-        val list = listOf<Cat>(
-            Cat("Mika", "Black-White", 3, "Fluffy"),
-            Cat("Puma", "Tiger-look", 8, "ShortHair")
-        )
-        val demoString : String = Gson().toJson(list)
+    private fun deserializeJsonString(resultText : String) : String{
+        return try {
+            val catType = object : TypeToken<List<Cat>>() {}.type
+            val cats = Gson().fromJson<List<Cat>>(resultText, catType)
+
+            cats.joinToString("\n")
+
+        } catch(ex : java.lang.Exception){
+            "Unable to deserialize Json. The following error occurred: $ex"
+        }
+    }
+
+    private fun onReset(){
+        tvResult?.text = ""
+        dataLoaded = false
     }
 }
